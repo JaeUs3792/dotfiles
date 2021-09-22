8a3bb1adb5db7f5649b465cb5a881926330ec75e
JaeYoo-Im <cpu3792@gmail.com>
2021-09-22 21:52:26 +0900

    xmonad bug fix.

--- .xmonad/scripts/autostart.sh
+++ .xmonad/scripts/autostart.sh
@@ -7,7 +7,7 @@ function run {
   fi
 }
 
-(sleep 2; run $HOME/.config/polybar/launch.sh) &
+#(sleep 2; run $HOME/.config/polybar/launch.sh) &
 
 # cursor
 xsetroot -cursor_name left_ptr &
@@ -29,3 +29,5 @@ picom -b --config $HOME/.xmonad/scripts/picom.conf &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 /usr/lib/xfce4/notifyd/xfce4-notifyd &
 seadrive-gui &
+
+trayer --edge top --align right --widthtype request --padding 6 --SetDockType true --SetPartialStrut true --expand true --monitor 0 --transparent true --alpha 0 --tint 0x282c34  --height 24 &
