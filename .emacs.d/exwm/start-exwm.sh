81a50beae42a1e0dc76fe2db9f55c6ec2de3fb42
JaeYoo-Im <cpu3792@gmail.com>
2021-10-08 17:30:33 +0900

    EXWM : exwm for wsl

--- .emacs.d/exwm/start-exwm.sh
+++ .emacs.d/exwm/start-exwm.sh
@@ -6,7 +6,9 @@
 #picom &
 #picom -b --config /home/jaeus/.config/i3/picom.conf &
 # Enable screen locking on suspend
-xss-lock -- slock &
-
-# Fire it up
+#xss-lock -- slock &
+# Fire it up for WSL
+VETHER_IP=$(/bin/grep nameserver /etc/resolv.conf 2> /dev/null | /bin/tr -s ' ' | /bin/cut -d' ' -f2)
+export DISPLAY=$VETHER_IP:10.0
+picom -b --config $HOME/.xmonad/scripts/picom.conf &
 exec dbus-launch --exit-with-session emacs -mm --debug-init -l ~/.emacs.d/desktop.el
