cd7eda45484f56001d4f5354e7d863934e5bfc9c
JaeYoo-Im <cpu3792@gmail.com>
2021-10-11 12:28:08 +0900

    Exwm : modified for labtop

--- .emacs.d/exwm/start-exwm.sh
+++ .emacs.d/exwm/start-exwm.sh
@@ -8,7 +8,7 @@
 # Enable screen locking on suspend
 #xss-lock -- slock &
 # Fire it up for WSL
-VETHER_IP=$(/bin/grep nameserver /etc/resolv.conf 2> /dev/null | /bin/tr -s ' ' | /bin/cut -d' ' -f2)
-export DISPLAY=$VETHER_IP:10.0
+#VETHER_IP=$(/bin/grep nameserver /etc/resolv.conf 2> /dev/null | /bin/tr -s ' ' | /bin/cut -d' ' -f2)
+#export DISPLAY=$VETHER_IP:10.0
 picom -b --config $HOME/.xmonad/scripts/picom.conf &
 exec dbus-launch --exit-with-session emacs -mm --debug-init -l ~/.emacs.d/desktop.el
