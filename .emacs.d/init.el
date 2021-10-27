35d09ed683499d986d5828bc4dd6278e8dea2a0f
JaeYoo-Im <cpu3792@gmail.com>
2021-10-27 18:10:08 +0900

    Emacs : Org mode templates for latex

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,16 +1,4 @@
 (setq vc-follow-symlinks t)
-(with-eval-after-load 'ox-latex
-(add-to-list 'org-latex-classes
-             '("org-plain-latex"
-               "\\documentclass{article}
-           [NO-DEFAULT-PACKAGES]
-           [PACKAGES]
-           [EXTRA]"
-               ("\\section{%s}" . "\\section*{%s}")
-               ("\\subsection{%s}" . "\\subsection*{%s}")
-               ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
-               ("\\paragraph{%s}" . "\\paragraph*{%s}")
-               ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))))
 (org-babel-load-file
  (expand-file-name
   "emacs.org"
