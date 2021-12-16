5e459d6235efa25571bcb3b368ef1154d5b6e8f8
JaeYoo-Im <cpu3792@gmail.com>
2021-12-16 10:57:38 +0900

    Wallpaper : crond added

--- .emacs.d/exwm/start-exwm.sh
+++ .emacs.d/exwm/start-exwm.sh
@@ -7,12 +7,8 @@
 #picom -b --config /home/jaeus/.config/i3/picom.conf &
 # Enable screen locking on suspend
 #xss-lock -- slock &
-# Fire it up for WSL
-VETHER_IP=$(/bin/grep nameserver /etc/resolv.conf 2> /dev/null | /bin/tr -s ' ' | /bin/cut -d' ' -f2)
-export DISPLAY=$VETHER_IP:10.0
-#export DISPLAY=192.168.144.1:10.0
 
 rm ~/.emacs.d/emacs.el
 picom -b --config $HOME/.xmonad/scripts/picom.conf &
-~/scripts/random_wallpaper.sh ~/ShareDirectory/wallpaper/karina/portrait
+~/scripts/random_wallpaper.sh
 exec dbus-launch --exit-with-session emacs -mm --debug-init -l ~/.emacs.d/desktop.el
--- scripts/random_wallpaper.sh
+++ scripts/random_wallpaper.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+#wal_dir=~/ShareDirectory/wallpaper/idol/landscape/karina
+#wal_dir=~/ShareDirectory/wallpaper/idol/portrait/karina
+wal_dir=~/ShareDirectory/wallpaper/normal
 
-files=($1/*)
+
+export DISPLAY=192.168.144.1:10.0
+files=($wal_dir/**/*)
 randomfile=$(printf "%s\n" "${files[RANDOM % ${#files[@]}]}")
 echo $randomfile
 wal -i $randomfile --saturate 1.0
