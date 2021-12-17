3585e81545675e4251df177c357c40bba7a97b57
JaeYoo-Im <cpu3792@gmail.com>
2021-12-17 14:34:04 +0900

    Wallpaper : random wallpaper improved.

--- scripts/random_wallpaper.sh
+++ scripts/random_wallpaper.sh
@@ -3,9 +3,16 @@
 #wal_dir=~/ShareDirectory/wallpaper/idol/portrait/karina
 wal_dir=~/ShareDirectory/wallpaper/normal
 
-
+# for wsl
 export DISPLAY=192.168.144.1:10.0
-files=($wal_dir/**/*)
+
+# random wallpaper
+files=()
+while IFS=  read -r -d $'\0'; do
+	files+=("$REPLY")
+done < <(find $wal_dir -type f -name "*" -print0)
 randomfile=$(printf "%s\n" "${files[RANDOM % ${#files[@]}]}")
 echo $randomfile
 wal -i $randomfile --saturate 1.0
+
+
