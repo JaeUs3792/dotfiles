71ad7970560591b44764fe99cf87072bbdba3040
JaeYoo-Im <cpu3792@gmail.com>
2022-09-17 02:09:55 +0900

    WM : config added.

--- .xmonad/scripts/autostart.sh
+++ .xmonad/scripts/autostart.sh
@@ -14,24 +14,27 @@ xsetroot -cursor_name left_ptr &
 
 # background
 #feh --bg-fill /usr/share/backgrounds/arcolinux/arco-wallpaper.jpg &
-dwall -p -s firewatch
+#dwall -p -s firewatch
+~/scripts/random_wallpaper.sh
 
 # other apps
-run variety &
+#run variety &
 run nm-applet &
-run pamac-tray &
-run xfce4-power-manager &
+#run pamac-tray &
+#run xfce4-power-manager &
 run volumeicon &
-run bitwarden-desktop &
-numlockx on &
-blueberry-tray &
+#run bitwarden-desktop &
+#numlockx on &
+#blueberry-tray &
+run lxsession &
 
 fcitx &
 picom -b --config $HOME/.xmonad/scripts/picom.conf &
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-/usr/lib/xfce4/notifyd/xfce4-notifyd &
+#/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+#/usr/lib/xfce4/notifyd/xfce4-notifyd &
 #seadrive-gui &
 
-trayer --edge top --align right --widthtype request --padding 6 --SetDockType true --SetPartialStrut true --expand true --monitor 0 --transparent true --alpha 0 --tint 0x282c34  --height 16 &
-
 emacs --daemon &
+
+sleep 2
+trayer --edge top --align right --widthtype request --padding 6 --SetDockType true --SetPartialStrut true --expand true --monitor 0 --transparent true --alpha 0 --tint 0x282c34  --height 16 &
