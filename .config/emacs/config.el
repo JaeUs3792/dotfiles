d23eb06cb31822cfc4bf5d5c77704b7dfaa0bf81
JaeYoo-Im <cpu3792@gmail.com>
2023-01-11 00:10:06 +0900

    remove backup file

--- .config/emacs/config.el
+++ .config/emacs/config.el
@@ -1,10 +1,14 @@
-(require 'crafted-defaults)		; Sensible Default settings for Emacs
-(require 'crafted-updates)     ; Tools to upgrade Crafted Emacs
-(require 'crafted-completion)	; selection framework based on 'vertico'
-(require 'crafted-ui)			; Better UI experience (modeline etc..)
-;;(require 'crafted-windows)     ; Window management configuration
-(require 'crafted-evil)		; An 'evil-mode' configuration
-;;(require 'custom-evil)			; An 'evil-mode' configuration
+(require 'crafted-defaults)     ; Sensible Default settings for Emacs
+(require 'crafted-updates)      ; Tools to upgrade Crafted Emacs
+(require 'crafted-completion)   ; selection framework based on 'vertico'
+(require 'crafted-ui)           ; Better UI experience (modeline etc..)
+(require 'crafted-evil)         ; An 'evil-mode' configuration
+;;(require 'custom-evil)        ; An 'evil-mode' configuration
+(require 'crafted-editing)      ; Whitespace trimming, auto parens etc.
+;;(require 'crafted-org)          ; org-appear, clickable hyperlinks etc.
+(require 'custom-org)          ; org-appear, clickable hyperlinks etc.
+(require 'crafted-project)
+(require 'crafted-latex)
 
 (setq-default tab-width 4)
 (defvaralias 'c-basic-offset 'tab-width)
@@ -29,12 +33,11 @@
 (add-hook 'emacs-startup-hook
           (lambda ()
             (custom-set-faces
-             `(default ((t (:font "Fira Code 10"))))
+             `(default ((t (:font "Fira Code 12"))))
              `(fixed-pitch ((t (:inherit (default)))))
              `(fixed-pitch-serif ((t (:inherit (default)))))
-             `(variable-pitch ((t (:font "Ubuntu 10")))))))
+             `(variable-pitch ((t (:font "Ubuntu Mono 12")))))))
 (set-fontset-font t `hangul (font-spec :name "NanumGothic"))
-
 ;; loading themes
 (crafted-package-install-package 'doom-themes)
 (progn
