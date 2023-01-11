9019d2e417dc3356066183defbf104c5d39c354c
JaeYoo-Im <cpu3792@gmail.com>
2023-01-12 02:14:49 +0900

    Emacs : completion done.

---
+++ .config/emacs/modules/custom-org.el
@@ -0,0 +1,8 @@
+(use-package valign
+  :defer t
+  :after (org markdown-mode)
+  ;; :hook ((org-mode markdown-mode) . valign-mode)
+  :custom ((valign-fancy-bar t)))
+
+(provide 'custom-org)
+;;; custom-org.el ends here
