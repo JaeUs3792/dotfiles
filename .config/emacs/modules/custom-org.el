11d63fd401914ec84177ec238e0953e618db6477
JaeYoo-Im <cpu3792@gmail.com>
2023-01-13 13:59:36 +0900

    Emacs : remove versioning target

--- .config/emacs/modules/custom-org.el
+++
@@ -1,8 +0,0 @@
-(use-package valign
-  :defer t
-  :after (org markdown-mode)
-  ;; :hook ((org-mode markdown-mode) . valign-mode)
-  :custom ((valign-fancy-bar t)))
-
-(provide 'custom-org)
-;;; custom-org.el ends here
