6436b9a2d192c4b2bf469f0aa36e67866171d4aa
JaeYoo-Im <cpu3792@gmail.com>
2023-10-27 15:46:35 +0900

    emacs : evil search include underscore

--- .config/emacs/lisp/init-evil.el
+++ .config/emacs/lisp/init-evil.el
@@ -25,7 +25,10 @@
   ;; eshell no evil
   (dolist (mode '(eshell-mode))
     (add-to-list 'evil-emacs-state-modes mode)))
-
+(with-eval-after-load 'evil
+    (defalias #'forward-evil-word #'forward-evil-symbol)
+    ;; make evil-search-word look for symbol rather than word boundaries
+    (setq-default evil-symbol-word-search t))
 (use-package evil-collection
   :defer nil
   :config
