508421f947617a93390e2546cd9588e19a2e7d1b
JaeYoo-Im <cpu3792@gmail.com>
2023-11-24 16:15:54 +0900

    emacs : nushell mode update

---
+++ .config/emacs/lisp/init-shell.el
@@ -0,0 +1,7 @@
+;;; init-eshell.el -*- lexical-binding: t -*-
+;; Emacs command shell
+
+(use-package nushell-mode)
+
+(provide 'init-shell)
+;;; init-eshell.el ends here
--- .config/emacs/lisp/init-verilog.el
+++ .config/emacs/lisp/init-verilog.el
@@ -11,40 +11,38 @@
   (setq verilog-indent-lists nil)
   (setq verilog-cexp-indent 4)
   (setq verilog-case-indent 4)
-  (setq verilog-auto-newline nil)
-  (use-package verilog-ext
-    :hook ((verilog-mode . verilog-ext-mode))
-    :init
-    ;; Can also be set through `M-x RET customize-group RET verilog-ext':
-    ;; Comment out/remove the ones you do not need
-    (when ON-WINDOWS
-        (setq verilog-ext-flycheck-linter 'verilog-iverilog)
-        (add-to-list 'exec-path "c:/iverilog/bin")
-        (add-to-list 'exec-path "c:/User/Users/AppData/Roaming/npm")
-        (setq flycheck-verilog-iverilog-executable "iverilog.exe")
-        (setq verilog-ext-eglot-svlangserver-bin-path "svlangserver"))
-    (setq verilog-ext-feature-list
-          '(font-lock
-            xref
-            capf
-            hierarchy
-            eglot
-            lsp
-            flycheck
-            beautify
-            navigation
-            template
-            formatter
-            compilation
-            imenu
-            which-func
-            hideshow
-            typedefs
-            time-stamp
-            block-end-comments
-            ports))
-    :config
-    (verilog-ext-mode-setup)))
+  (setq verilog-auto-newline nil))
+(use-package verilog-ext
+  :hook ((verilog-mode . verilog-ext-mode))
+  :init
+  ;; Can also be set through `M-x RET customize-group RET verilog-ext':
+  ;; Comment out/remove the ones you do not need
+  (when ON-WINDOWS ;; on-linux verilator
+    (setq verilog-ext-flycheck-linter 'verilog-iverilog)
+    (add-to-list 'exec-path "c:/iverilog/bin")
+    (setq flycheck-verilog-iverilog-executable "iverilog.exe"))
+  (setq verilog-ext-feature-list
+        '(font-lock
+          xref
+          ;; capf
+          hierarchy
+          eglot
+          ;; lsp
+          flycheck
+          beautify
+          navigation
+          template
+          formatter
+          compilation
+          imenu
+          which-func
+          hideshow
+          typedefs
+          time-stamp
+          block-end-comments
+          ports))
+  :config
+  (verilog-ext-mode-setup))
 
 (provide 'init-verilog)
 ;;; init-verilog.el ends here
