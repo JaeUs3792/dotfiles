94276e913d7799054b3803bf4979952c530f13d0
JaeYoo-Im <cpu3792@gmail.com>
2024-04-11 21:17:39 +0900

    random wallpaper env selection.

--- scripts/random_wallpaper.sh
+++ scripts/random_wallpaper.sh
@@ -4,17 +4,28 @@
 wal_dir=~/walls/normal
 
 # random wallpaper
-files=()
-while IFS=  read -r -d $'\0'; do
-    files+=("$REPLY")
-done < <(find $wal_dir -type f -name "*" -print0)
-randomfile=$(printf "%s\n" "${files[RANDOM % ${#files[@]}]}")
-echo $randomfile
+if [[ $JUPCID == "HOME-DESKTOP" ]]; then
+	files=()
+	while IFS=  read -r -d $'\0'; do
+		files+=("$REPLY")
+	done < <(find $wal_dir -type f -name "*" -print0)
+	randomfile=$(printf "%s\n" "${files[RANDOM % ${#files[@]}]}")
+	echo $randomfile
 
-files=()
-while IFS=  read -r -d $'\0'; do
-    files+=("$REPLY")
-done < <(find $wal_dir -type f -name "*" -print0)
-randomfile2=$(printf "%s\n" "${files[RANDOM % ${#files[@]}]}")
-echo $randomfile2
-feh --bg-max "$randomfile" --bg-max "$randomfile2"
+	files=()
+	while IFS=  read -r -d $'\0'; do
+		files+=("$REPLY")
+	done < <(find $wal_dir -type f -name "*" -print0)
+	randomfile2=$(printf "%s\n" "${files[RANDOM % ${#files[@]}]}")
+	echo $randomfile2
+	feh --bg-max "$randomfile" --bg-max "$randomfile2"
+else
+	files=()
+	while IFS=  read -r -d $'\0'; do
+		files+=("$REPLY")
+	done < <(find $wal_dir -type f -name "*" -print0)
+	randomfile=$(printf "%s\n" "${files[RANDOM % ${#files[@]}]}")
+	echo $randomfile
+	feh --bg-max "$randomfile"
+fi
+
