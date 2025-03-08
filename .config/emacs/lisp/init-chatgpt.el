8e17f130e70545bff1b14d5f9f2f4dabbc240138
JaeYoo-Im <cpu3792@gmail.com>
2025-03-08 12:08:53 +0900

    copilot & gpt packages add

--- .config/emacs/lisp/init-chatgpt.el
+++ .config/emacs/lisp/init-chatgpt.el
@@ -7,6 +7,17 @@
   (setq gptel-model "gpt-4o-mini")
   (setq gptel-api-key custom-chatgpt-my-key))
 
+(use-package org-ai
+  :straight t
+  :ensure t
+  :commands (org-ai-mode
+             org-ai-global-mode)
+  :init
+  (add-hook 'org-mode-hook #'org-ai-mode) ; enable org-ai in org-mode
+  (org-ai-global-mode) ; installs global keybindings on C-c M-a
+  :config
+  (setq org-ai-default-chat-model "gpt-4") ; if you are on the gpt-4 beta:
+  (org-ai-install-yasnippets)) ; if you are using yasnippet and want `ai` snippets
 
 (provide 'init-chatgpt)
 ;;; init-chatgpt.el ends here
