f8d47693c9753f02cb7f243c0d124818af60bc63
JaeYoo-Im <cpu3792@gmail.com>
2025-05-22 22:47:19 +0900

    script : init linux

--- scripts/init_script_archlinux.sh
+++ scripts/init_script_archlinux.sh
@@ -1,33 +1,27 @@
-#!/bin/sh
-paru
-paru -S stow trayer --noconfirm
+#!/bin/bash
 
-# config file handling
-rm ~/.config/polybar/config
-rm ~/.zshrc ~/.bashrc ~/.xmonad/xmonad.hs
-rm -rf ~/.config/alacritty ~/.xmonad/scripts
+# essential packages
+yay -S eza --noconfirm
+
+# clone config
+git clone https://github.com/JaeUs3792/dotfiles ~/.dotfiles
+
+yay -S stow --noconfirm
 cd ~/.dotfiles
 stow .
 
+# korean input
+yay -S ibus-hangul
 
 #ZSH
+yay -S zsh starship --noconfirm
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 mv ~/.zshrc.pre-oh-my-zsh ~/.zshrc
 
-# NUSHELL
-#paru -S nushell
-#nu ~/scripts/init_script_nushell.nu
-#echo "Changing Shell to nushell"
-#chsh -s /bin/nu   # change shell
-
-# FISH
-#paru -S fish starship
-#chsh -s /bin/fish
-
 # neovim
-paru -S neovim --noconfirm
+yay -S neovim --noconfirm
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 nvim +PlugInstall +q +q
@@ -38,32 +32,27 @@ git config --global user.email "cpu3792@gmail.com"
 git config --global user.autocrlf input
 
 # tmux
-paru -S tmux --noconfirm
+yay -S tmux --noconfirm
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 TMUX_PLUGIN_MANAGER_PATH=~/.config/tmux/plugins/tpm ~/.tmux/plugins/tpm/scripts/install_plugins.sh
 TMUX_PLUGIN_MANAGER_PATH=~/.config/tmux/plugins/tpm ~/.tmux/plugins/tpm/bin/update_plugins all
 
 # font
-paru -S ttf-firacode-nerd ttf-nanum ttf-mononoki-nerd --noconfirm
-
-# hangul
-paru -S fcitx-hangul fcitx-configtool --noconfirm
-
-# enpass
-#paru -S enpass --noconfirm
-#paru -S bitwarden --noconfirm
-#paru -S green-tunnel --noconfirm
-
-#emacs
-paru -S ripgrep emacs --noconfirm
-paru -S texlive-most texlive-lang --noconfirm
-paru -S jupyterlab --noconfirm
-paru -S zathura zathura-cb zathura-pdf-mupdf
+yay -S ttf-firacode-nerd ttf-momonoki-nerd otf-comicshanns-nerd ttf-times-new-roman ttf-nanum noto-fonts-emoji ttf-symbola noto-font-cjk --noconfirm
 
 #syncthing
-paru -S syncthing --noconfirm
-mkdir SyncThing
+yay -S syncthing --noconfirm
 sudo systemctl enable syncthing@jaeus.service
+sudo systemctl start syncthing@jaeus.service
 
-# etc
-paru -S figlet --noconfirm
+#emacs
+yay -S emacs --noconfirm
+paru -S texlive-basic texlive-langkorean --noconfirm
+paru -S jupyterlab --noconfirm
+paru -S zathura-pdf-mupdf
+
+#verilator
+yay -S verilator
+
+# trilium
+yay -S trilium-bin
---
+++ scripts/vivado_installlibs.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+yay -S ncurses5-compat-libs libxcrypt-compat libpng12 lib32-libpng12 gtk3 inetutils xorg-xlsclients cpio
