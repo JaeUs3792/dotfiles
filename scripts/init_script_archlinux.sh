db93b2c85d568395b01220a5fa18ee4714fb20e4
JaeYoo-Im <cpu3792@gmail.com>
2025-05-25 23:11:28 +0900

    script : endeavouros to cachyOS

--- scripts/init_script_archlinux.sh
+++ scripts/init_script_archlinux.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 
 # essential packages
-yay -S eza --noconfirm
+paru -S eza --noconfirm
 
 # clone config
 git clone https://github.com/JaeUs3792/dotfiles ~/.dotfiles
 
-yay -S stow --noconfirm
+paru -S stow --noconfirm
 cd ~/.dotfiles
 stow .
 
 # korean input
-yay -S ibus-hangul
+paru -S ibus-hangul
 
 #ZSH
-yay -S zsh starship --noconfirm
+paru -S zsh starship --noconfirm
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 mv ~/.zshrc.pre-oh-my-zsh ~/.zshrc
 
+# fish
+paru -S fish starship autin bat --noconfirm
+
+
 # neovim
-yay -S neovim --noconfirm
+paru -S neovim --noconfirm
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 nvim +PlugInstall +q +q
@@ -32,27 +36,35 @@ git config --global user.email "cpu3792@gmail.com"
 git config --global user.autocrlf input
 
 # tmux
-yay -S tmux --noconfirm
+paru -S tmux --noconfirm
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 TMUX_PLUGIN_MANAGER_PATH=~/.config/tmux/plugins/tpm ~/.tmux/plugins/tpm/scripts/install_plugins.sh
 TMUX_PLUGIN_MANAGER_PATH=~/.config/tmux/plugins/tpm ~/.tmux/plugins/tpm/bin/update_plugins all
 
 # font
-yay -S ttf-firacode-nerd ttf-momonoki-nerd otf-comicshanns-nerd ttf-times-new-roman ttf-nanum noto-fonts-emoji ttf-symbola noto-font-cjk --noconfirm
+paru -S ttf-firacode-nerd ttf-mononoki-nerd otf-comicshanns-nerd ttf-times-new-roman ttf-nanum noto-fonts-emoji ttf-symbola noto-fonts-cjk --noconfirm
 
 #syncthing
-yay -S syncthing --noconfirm
+paru -S syncthing --noconfirm
 sudo systemctl enable syncthing@jaeus.service
 sudo systemctl start syncthing@jaeus.service
 
+# express vpn
+paru -S expressvpn-gui
+sudo systemctl enable expressvpn
+sudo systemctl start expressvpn
+
 #emacs
-yay -S emacs --noconfirm
+paru -S emacs --noconfirm
 paru -S texlive-basic texlive-langkorean --noconfirm
 paru -S jupyterlab --noconfirm
 paru -S zathura-pdf-mupdf
 
 #verilator
-yay -S verilator
+paru -S verilator
 
 # trilium
-yay -S trilium-bin
+paru -S trilium-bin
+
+# sourcegit
+paru -S sourcegit-bin
