787680f1b56cbd9e6a9e45f809c9d7b3d03b79fd
JaeYoo-Im <cpu3792@gmail.com>
2025-12-24 00:17:27 +0900

    emacs : cape-updated.

--- .config/emacs/lisp/init-vertico.el
+++ .config/emacs/lisp/init-vertico.el
@@ -186,9 +186,6 @@
   ;; Silence the pcomplete capf, no errors or messages!
   ;; Important for corfu
   (advice-add 'pcomplete-completions-at-point :around #'cape-wrap-silent)
-  ;; Ensure that pcomplete does not write to the buffer
-  ;; and behaves as a pure `completion-at-point-function'.
-  (advice-add 'pcomplete-completions-at-point :around #'cape-wrap-purify)
   (add-hook 'eshell-mode-hook
             (lambda () (setq-local corfu-quit-at-boundary t
                                    corfu-quit-no-match t
